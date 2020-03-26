4b271df67ed8012d9cd18fa53dc7199588202d64
Mark Barnes <msbarnes@ualr.edu>
2020-03-26 15:52:06 -0500

    Moved from old repo

---
+++ Advanced Game Programming/Advanced Game Programming/Source/Library.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+#include "tinyxml2.h"
+#include "Library.h"
+#include "ObjectFactory.h"
+#include "Texture.h"
+#include "GraphicsDevice.h"
+#include "InputDevice.h"
+#include "PhysicsDevice.h"
+#include "Object.h"
+#include "AudioDevice.h"
+#include "Sound.h"
+
+// Populate object and art libraries
+Library::Library(GraphicsDevice* gDevice, InputDevice* iDevice, PhysicsDevice* pDevice, AudioDevice* aDevice): 
+	gDevice(gDevice), 
+	iDevice(iDevice), 
+	pDevice(pDevice),
+	aDevice(aDevice) {
+
+	tinyxml2::XMLDocument doc;
+
+	// Load assets XML file
+	if (doc.LoadFile("Assets/Assets.xml") != tinyxml2::XML_SUCCESS) {
+		printf("Bad File Path");
+		exit(1);
+	}
+
+	tinyxml2::XMLElement* root = doc.FirstChildElement("Assets");
+
+	if (!root) {
+		printf("Error");
+	}
+
+	// Populate artLibrary map with sprites
+	for (tinyxml2::XMLElement* element = root->FirstChildElement("Sprite"); element; element = element->NextSiblingElement("Sprite")) {
+		artLibrary[element->Attribute("name")] = std::make_shared<Texture>(gDevice, element->Attribute("path"));
+	}
+
+	// Populate SFXLibrary with sounds
+	for (tinyxml2::XMLElement* element = root->FirstChildElement("Sound"); element; element = element->NextSiblingElement("SFX")) {
+		SFXLibrary[element->Attribute("name")] = std::make_unique<SoundEffect>(element->Attribute("path"), aDevice);
+	}
+
+	// Populate musicLibrary with songs
+	for (tinyxml2::XMLElement* element = root->FirstChildElement("Song"); element; element = element->NextSiblingElement("Song")) {
+		musicLibrary[element->Attribute("name")] = std::make_unique<BackgroundMusic>(element->Attribute("path"), aDevice);
+	}
+}
+
+Library::~Library() {
+	artLibrary.clear();
+}
+
+void Library::addObject(std::unique_ptr<Object> ) {
+
+}
+
+std::vector<Object*> Library::getObjects(ObjectType type) {
+	std::vector<Object*> objectGet;
+	
+	
+	return objectGet;
+}
